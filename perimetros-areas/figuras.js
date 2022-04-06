e4fcc4c0ec3102519baeb4f853712405115bfd2b
Dario Paladines <dariopaladines.23@gmail.com>
2022-04-05 19:36:19 -0500

    Calculando el perimetro y area del cuadrado

---
+++ perimetros-areas/figuras.js
@@ -0,0 +1,16 @@
+
+//CUADRADO///////////////
+
+console.group("Cuadrado")
+// const lado = 5;
+// console.log("los lados del cuadrado son " + lado + "cm")
+
+function perimetroC(lado){
+    return lado * 4;
+} 
+
+
+function areaC(lado){
+    return lado **2;
+ } 
+console.groupEnd();
