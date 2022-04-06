88df78bf7ab81b169e32e4f23766a220402bd540
Dario Paladines <dariopaladines.23@gmail.com>
2022-04-05 19:37:26 -0500

    Calculando el perimetro y área del triangulo

--- perimetros-areas/figuras.js
+++ perimetros-areas/figuras.js
@@ -14,3 +14,22 @@ function areaC(lado){
     return lado **2;
  } 
 console.groupEnd();
+
+///TRIANGULO////////////////////////////////////
+
+console.group("Triangulo")
+// const lado1 = 6; 
+// const lado2 = 6;
+// const base = 4;
+// const altura = 5.5;
+
+
+function perimetroT(lado111, lado222, base11){
+    return  lado111 + lado222 + base11;
+} 
+
+function areaT(base, altura){
+    return (base * altura) / 2;
+} 
+
+console.groupEnd();
